9c0936a69889b74db9c857b13a1fc54e61ffe487
kylieanyce <kylieanyce@gmail.com>
2021-05-10 15:34:08 -0500

    added profile provider to bring back individual profile view

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,8 +4,10 @@ import { GameList } from "./game/GameList.js"
 import { EventList } from "./game/EventList.js"
 import { GameProvider } from "./game/GameProvider.js"
 import { EventProvider } from "./game/EventProvider.js"
+import { ProfileProvider } from "./auth/ProfileProvider.js"
 import { GameForm } from "./game/GameForm.js"
 import { EventForm } from "./game/EventForm.js"
+import { Profile } from "./auth/Profile.js"
 
 export const ApplicationViews = () => {
     return <>
@@ -15,21 +17,26 @@ export const ApplicationViews = () => {
         }}>
             <GameProvider>
                 <EventProvider>
-                    <Route exact path="/">
-                        <GameList />
-                    </Route>
-                    <Route path="/games">
-                        <GameList />
-                    </Route>
-                    <Route exact path="/games/new">
-                        <GameForm />
-                    </Route>
-                    <Route path="/events">
-                        <EventList />
-                    </Route>
-                    <Route exact path="/events/new">
-                        <EventForm />
-                    </Route>
+                    <ProfileProvider>
+                        <Route exact path="/">
+                            <GameList />
+                        </Route>
+                        <Route path="/games">
+                            <GameList />
+                        </Route>
+                        <Route exact path="/games/new">
+                            <GameForm />
+                        </Route>
+                        <Route path="/events">
+                            <EventList />
+                        </Route>
+                        <Route exact path="/events/new">
+                            <EventForm />
+                        </Route>
+                        <Route exact path="/profile">
+                            <Profile />
+                        </Route>
+                    </ProfileProvider>
                 </EventProvider>
             </GameProvider>
         </main>
