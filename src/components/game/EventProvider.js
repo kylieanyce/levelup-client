8e4a51f01a6a21d42b8037a2306adbaa4b814f90
kylieanyce <kylieanyce@gmail.com>
2021-05-10 11:09:13 -0500

    commented out json res in leaveEvent so button functions properly and changes color correctly

--- src/components/game/EventProvider.js
+++ src/components/game/EventProvider.js
@@ -35,7 +35,7 @@ export const EventProvider = (props) => {
                 "Authorization": `Token ${localStorage.getItem("lu_token")}`
             }
         })
-            .then(response => response.json())
+            // .then(response => response.json())
             .then(getEvents)
     }
 
